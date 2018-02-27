d7a97a81cc8d00af2a4e930ff2d529d233f2ab64
Chase Bowden and Maggie Summers <chase@>
2018-02-27 14:05:25 -0800

    add delete and update functionality

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ require("pry")
 get('/') do
   @employees = Employee.all()
   @projects = Project.all()
+  binding.pry
   erb(:home)
 end
 
@@ -36,16 +37,36 @@ end
 
 get('/projects/:id') do
   @project = Project.find(params.fetch("id").to_i())
-  @employees = Employee.all()
+  @available_employees = Employee.all() - @project.employees
   erb(:project)
 end
 
-patch('/projects/:id')do
+post('/projects/:id')do
   @project = Project.find(params.fetch("id").to_i)
   new_employee = Employee.find(params.fetch("employee_id"))
   @project.employees.push(new_employee)
-  @employees = Employee.all()
+  @available_employees = Employee.all() - @project.employees
   erb(:project)
+end
+
+get('/projects/:id/edit') do
+  @project = Project.find(params.fetch("id").to_i)
+  erb(:edit_project)
+end
+
+patch('/projects/:id') do
+  title = params.fetch("title")
+  @project = Project.find(params.fetch("id").to_i)
+  @project.update({:title => title})
+  @available_employees = Employee.all() - @project.employees
+  erb(:project)
+end
+
+delete('/projects/:id') do
+  @project = Project.find(params.fetch("id").to_i)
+  @project.delete()
+  @projects = Project.all()
+  erb(:projects)
 end
 
 get('/employees') do
@@ -67,10 +88,31 @@ get('/employees/:id')do
   erb(:employee)
 end
 
-patch('/employees/:id')do
+post('/employees/:id')do
   @employee = Employee.find(params.fetch("id").to_i)
   new_project = Project.find(params.fetch("project_id"))
   @employee.projects.push(new_project)
   @available_projects = Project.all() - @employee.projects
   erb(:employee)
+end
+
+get('/employees/:id/edit') do
+  @employee = Employee.find(params.fetch("id").to_i)
+  erb(:edit_employee)
+end
+
+patch('/employees/:id')do
+  name = params.fetch("name")
+  @employee = Employee.find(params.fetch("id").to_i)
+  @employee.update({:name => name})
+  @employees = Employee.all()
+  @available_projects = Project.all() - @employee.projects
+  erb(:employee)
+end
+
+delete('/employees/:id') do
+  @employee = Employee.find(params.fetch("id").to_i)
+  @employee.delete()
+  @employees = Employee.all()
+  erb(:employees)
 end
